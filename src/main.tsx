67bf3bc640e3404b38178d64d56e94b925422407
kwanghe <104612684+kwanghe@users.noreply.github.com>
2024-10-28 09:08:06 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/minitest-user1">
       <App />
     </BrowserRouter>
   </StrictMode>
