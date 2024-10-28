ccb858e3633dad614f1d8256acebc720900c5596
kwanghe <104612684+kwanghe@users.noreply.github.com>
2024-10-28 09:12:14 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/minitest-user1/', // GitHub Pages 배포 경로 설정
+
 });
